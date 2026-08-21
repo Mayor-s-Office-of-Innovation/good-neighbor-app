e4b9171f117c76498661c95323ffbac3d295ec9b
Aaron Hans <aaronhans@gmail.com>
2026-08-20 17:26:27 -0700

    swap components for code entry

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -39,6 +39,7 @@ import "./styles/wa-awesome.css";
 import "@awesome.me/webawesome/dist/components/button/button.js";
 import "@awesome.me/webawesome/dist/components/icon/icon.js";
 import "@awesome.me/webawesome/dist/components/input/input.js";
+import "@awesome.me/webawesome/dist/components/otp-input/otp-input.js";
 import "@awesome.me/webawesome/dist/components/textarea/textarea.js";
 import "@awesome.me/webawesome/dist/components/checkbox/checkbox.js";
 import "@awesome.me/webawesome/dist/components/select/select.js";
